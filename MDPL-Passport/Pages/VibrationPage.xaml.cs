d0030eb2c8f42bdde5684543c1061127d44c2914
Bohdan <bohdanvoroniak455@gmail.com>
2023-09-02 19:08:18 +0300

    add vibration

--- MDPL-Passport/Pages/VibrationPage.xaml.cs
+++ MDPL-Passport/Pages/VibrationPage.xaml.cs
@@ -7,10 +7,39 @@ public partial class VibrationPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void StartVibration(object sender, EventArgs e)
+    private void DefaultVibration(object sender, EventArgs e)
     {
+        Vibration.Default.Cancel();
         const int secondsToVibrate = 5;
         TimeSpan vibrationLength = TimeSpan.FromSeconds(secondsToVibrate);
         Vibration.Default.Vibrate(vibrationLength);
+    }
+
+    private async void PremiumVibration(object sender, EventArgs e)
+    {
+        Vibration.Default.Cancel();
+
+        for (int i = 0; i < 5; i++)
+        {
+            TimeSpan vibrationLength = TimeSpan.FromSeconds(1);
+            Vibration.Default.Vibrate(vibrationLength);
+            await Task.Delay(1000);
+        }
+    }
+
+    private async void CockuldVibration(object sender, EventArgs e)
+    {
+        Vibration.Default.Cancel();
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Vibration.Default.Vibrate(100);
+                await Task.Delay(100);
+            }
+            Vibration.Default.Vibrate(1000);
+            await Task.Delay(1000);
+        }
     }
 }
